1e10ba01ded12da5a3e60ba5aeb536b23a930bd0
Shadi <karbalhandishadi@gmail.com>
2024-06-25 14:05:40 +0200

    yep

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        for ($i = 0; $i < 1; $i++) {
+        for ($i = 0; $i < 25; $i++) {
             DB::table('users')->insert([
                 'avatar' => $faker->randomElement(['assets/images/student_f.png', 'assets/images/student_m.png', 'assets/images/teacher_m.png', 'assets/images/teacher_f.png']),
                 'name' => $faker->firstName,
@@ -33,6 +33,26 @@ class UserSeeder extends Seeder
                 'adress' => $faker->address,
                 'phone' => $faker->phoneNumber,
                 'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        for ($i = 0; $i < 1; $i++) {
+            DB::table('users')->insert([
+                'avatar' => 'assets/images/teacher_m.png',
+                'name' => 'Abdullah',
+                'surname1' => 'Saleh',
+                'surname2' => 'Raidi',
+                'email' => 'admin@ilmacademy.com',
+                'dni' => $faker->unique()->numerify('#########A'),
+                'birthdate' => $faker->date('Y-m-d', '2000-12-31'),
+                'gender' => $faker->randomElement(['male', 'female']),
+                'rol' => 'Administrador',
+                'adress' => $faker->address,
+                'phone' => $faker->phoneNumber,
+                'password' => Hash::make('12345678'),
                 'remember_token' => Str::random(10),
                 'created_at' => now(),
                 'updated_at' => now(),
